dfd99f6ee12c271164e0ad72b59175bb70c480f7
Michael Douglas B. Araujo <michaeldouglas010790@gmail.com>
2022-12-14 21:39:20 -0300

    Plataformas cognitivas - ajustes

--- PlataformasCognitivas/FlaskGoogleDocker/main.py
+++ PlataformasCognitivas/FlaskGoogleDocker/main.py
@@ -1,13 +1,18 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from src.controllers.predict import Predict
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+def index():
+    return "Hello API -"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """Predict route"""
+    return Predict.execute(request.json)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
